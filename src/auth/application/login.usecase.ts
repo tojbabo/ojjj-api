4df45e2ac5a466df91cbd7bfcb5ba23b54377cf7
one joon <tojbabo@gmail.com>
2026-03-31 00:36:37 +0900

    feat(token): add create access token and refresh token, sending

--- src/auth/application/login.usecase.ts
+++ src/auth/application/login.usecase.ts
@@ -1,11 +1,15 @@
 import * as jwt from 'jsonwebtoken';
 
-function makeToken(id: string, hour:number) {
-  const token = jwt.sign(
-    { id },                 // payload
-    process.env.JWT_KEY!,   // secret
-    { expiresIn: `${hour}h` }     // optional
-  );
+export class AuthUsecase {
+  constructor(){}
 
-  return token;
+  makeToken(id: string, hour:number) {
+    const token = jwt.sign(
+      { id },                 // payload
+      process.env.JWT_KEY!,   // secret
+      { expiresIn: `${hour}h` }     // optional
+    );
+
+    return token;
+  }
 }
--- src/auth/presentation/auth.controller.ts
+++ src/auth/presentation/auth.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller,UnauthorizedException, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { AppService } from '../../app.service';
 import {DynamoDBService} from '../../dynamodb.service';
 import {logger} from '../../utils/logger';
+import { AuthUsecase } from '../application/login.usecase';
 
 
 
 @Controller('api/auth')
 export class AuthController {
-  constructor(private readonly appService: AppService, private readonly dynamoDBService: DynamoDBService) {}
+  constructor(
+    private readonly appService: AppService, 
+    private readonly dynamoDBService: DynamoDBService,
+    private readonly usecase: AuthUsecase
+  ) {}
 
   @Post('/login')
   async authlogin(@Body() body: {id:string, pw:string}): Promise<number> {
@@ -26,7 +32,7 @@ export class AuthController {
   }
 
   @Post('/join')
-  async authjoin(@Body() body: {id:string, pw:string}): Promise<number> {
+  async authjoin(@Body() body: {id:string, pw:string}, @Res({passthrough:true}) res: Response): Promise<any> {
     logger.info(`api/auth/join`);
     const {id, pw} = body;
     const pwhash = await this.appService.pwHash(pw)
@@ -34,7 +40,21 @@ export class AuthController {
 
     if(!isSuccess){
       await this.dynamoDBService.joinUser(id, pwhash)
-      return 1
+
+      const shorttoken = await this.usecase.makeToken(id, 1);
+      const longtoken = await this.usecase.makeToken(id, 24*30);
+
+
+      res.cookie('refreshToken', longtoken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      });
+
+      return {
+        shorttoken
+      };
     }
     else{
       return 0
